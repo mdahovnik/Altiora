3811086ff4df0310186a97991998530c902eeb9e
Lina Kalinka <lina.kalinka@gmail.com>
2025-07-17 19:12:00 +0300

    feat: add all types of inputs

--- source/entities/trash-ui-entity/ui/_ui-list/ui/ui-list.tsx
+++ source/entities/trash-ui-entity/ui/_ui-list/ui/ui-list.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import { UIButton } from '../../button'
+import Input from '../../input'
 import classes from '../styles/styles.module.scss'
 
 export const UIList: React.FC = () => {
   return (
     <div className={classes.container}>
       <UIButton />
-
+      <Input label="" supportingText="инпут" value="" onClear={() => {}} />
+      <Input label="" supportingText="инпут с ошибкой" value="" error onClear={() => {}} />
       {/* Добавляем компоненты ниже */}
     </div>
   )
